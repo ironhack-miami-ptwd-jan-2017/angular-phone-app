3bcdb8afbf99744db2c2f8d7fd420ae657ceb7dc
Javid Shamloo <javidshamloo@Javids-MBP.attlocal.net>
2017-06-28 00:47:32 -0400

    phone app beginnings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PhoneDetailsComponent } from './phone-details/phone-details.component';
+import { PhoneListComponent } from './phone-list/phone-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhoneDetailsComponent,
+    PhoneListComponent
   ],
   imports: [
     BrowserModule
